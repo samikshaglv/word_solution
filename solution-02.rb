3b2c5354045fb9b68ca6be4bdd2f6f81dc37e13b
Samiksha Galav <samikshaglv@gmail.com>
2020-11-09 17:02:23 +0530

    Update solution-02.rb

--- solution-02.rb
+++ solution-02.rb
@@ -1,7 +1,5 @@
-class FibonnaciSequence
-	def fibonnaci(n)
-		return n if(0..1).include?(n)
-		 fibonnaci(n-1) + fibonnaci(n-2)
-	end
-	print fibonnaci(n)
+def fibonnaci(n)
+  return n if(0..1).include?(n)
+  fibonnaci(n-1) + fibonnaci(n-2)
 end
+print fibonnaci(n)
