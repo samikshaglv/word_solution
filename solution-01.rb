e22e2eaa8f12fdfc51dcbb1537cef6a24e2b6201
samikshaglv <samikshaglv@gmail.com>
2020-11-09 16:30:52 +0530

    Added new files

---
+++ solution-01.rb
@@ -0,0 +1,8 @@
+class String
+  def reverse(str)
+  	arr_s = str.split("")
+  	reversed = []
+  	arr_s.size.times{reversed << arr_s.pop}
+  	reversed.join("")
+   end
+end
---
+++ solution-02.rb
@@ -0,0 +1,7 @@
+class FibonnaciSequence
+	def fibonnaci(n)
+		return n if(0..1).include?(n)
+		 fibonnaci(n-1) + fibonnaci(n-2)
+	end
+	print fibonnaci(n)
+end
